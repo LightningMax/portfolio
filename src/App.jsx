ff4c63ffb6bd5a47f400fccecea3dde864269702
Max Reis <maxreis507@gmail.com>
2025-04-01 17:19:40 +0200

    Improve file structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/homepage/Hero";
+import NavBar from "./components/ui/NavBar";
+
+export default function App() {
+  return (
+    <div className="bg-nier-50">
+      <header>
+        <NavBar />
+      </header>
+        <Hero />
+      <main>
+      </main>
+    </div>
+  );
+}
--- src/components/CTAButton.jsx
+++
@@ -1,6 +0,0 @@
-export default function CTAButton({message}) {
-
-    return (
-        <button className="bg-nier-400 text-nier-50 px-4 py-2 rounded-4xl" >{message}</button>
-    )
-}
